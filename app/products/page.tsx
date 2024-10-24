ad61c356d327d6b0a83bf09d706ebdd23bf79899
Wilian Brito <contato.beitsoft@gmail.com>
2024-10-24 14:38:17 -0300

    feat: add products table

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,29 @@
-function Prodcuts() {
-  return <div>Products Page</div>;
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumn } from "./_components/table-columns";
+
+async function ProductsPage() {
+  const products = await db.product.findMany({});
+
+  //consigo aqui chamar o banco de dados
+  return (
+    <div className="m-8 w-full space-y-8 bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        <Button className="gap-2">
+          <PlusIcon size={20} /> Novo Produto
+        </Button>
+      </div>
+      <DataTable columns={productTableColumn} data={products} />
+    </div>
+  );
 }
 
-export default Prodcuts;
+export default ProductsPage;
